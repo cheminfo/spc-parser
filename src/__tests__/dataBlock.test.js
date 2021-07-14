7f9a308e70ad60d5d49e7b3ee71cdfb34e7891b3
Benjamin Yiu <benjamin.yiu@protonmail.com>
2021-07-14 11:31:38 +0200

    chore: Add tests for dataBlock

--- src/__tests__/dataBlock.test.js
+++ src/__tests__/dataBlock.test.js
@@ -13,5 +13,8 @@ const ramanBuffer = new IOBuffer(fs.readFileSync(`${pathFiles}RAMAN.SPC`));
 const raman = readDataBlock(ramanBuffer, mainHeader(ramanBuffer));
 test('data block parsing test', () => {
   expect(mXyxy).toHaveLength(512);
-  expect(raman).toHaveLength(1);
+  expect(mXyxy[0].y[1]).toStrictEqual(3188);
+  expect(raman[0].x[1]).toStrictEqual(3994.8946331825773);
+  //expect(mXyxy[0].x[2]).toStrictEqual(16931.1999969);
+  expect(raman[0].y[1]).toStrictEqual(0.0186002254486084);
 });
