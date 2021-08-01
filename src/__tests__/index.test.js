a4288a55ba8faffe39a823a01b74efacc8d18464
Luc Patiny <luc@patiny.com>
2021-08-01 12:31:47 +0200

    feat!: Parse use directly a buffer

---
+++ src/__tests__/index.test.js
@@ -0,0 +1,11 @@
+import { readFileSync } from 'fs';
+import { join } from 'path';
+
+import { parse } from '..';
+
+test('parse', () => {
+  const buffer = readFileSync(join(__dirname, 'files', 'Ft-ir.spc'));
+  const result = parse(buffer);
+  expect(result.spectra).toHaveLength(1);
+  expect(Object.keys(result.meta)).toHaveLength(31);
+});
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import { IOBuffer } from 'iobuffer';
+
 import { readDataBlock } from './dataBlock';
 import { readLogBlock } from './logBlock';
 import { mainHeader } from './mainHeader';
@@ -8,10 +10,11 @@ import { mainHeader } from './mainHeader';
  * @return {object} Object containing every information contained in the SPC file
  */
 export function parse(buffer) {
-  const meta = mainHeader(buffer);
-  const spectra = readDataBlock(buffer, meta);
+  const ioBuffer = new IOBuffer(buffer);
+  const meta = mainHeader(ioBuffer);
+  const spectra = readDataBlock(ioBuffer, meta);
   if (meta.logOffset && meta.logOffset !== 0) {
-    return { meta, spectra, logs: readLogBlock(buffer, meta.logOffset) };
+    return { meta, spectra, logs: readLogBlock(ioBuffer, meta.logOffset) };
   }
   return { meta, spectra };
 }
