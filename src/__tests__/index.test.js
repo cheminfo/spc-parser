e65bb9909495dbf516cc209615ec59c4a49d6562
Luc Patiny <luc@patiny.com>
2021-08-02 14:47:08 +0200

    test: add variable test for index

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -6,6 +6,12 @@ import { parse } from '..';
 test('parse', () => {
   const buffer = readFileSync(join(__dirname, 'data', 'Ft-ir.spc'));
   const result = parse(buffer);
+  expect(result.spectra[0].variables[0]).toMatchObject({
+    symbol: 'x',
+    label: 'Wavenumber',
+    units: 'cm-1',
+    type: 'INDEPENDENT',
+  });
   expect(result.spectra).toHaveLength(1);
   expect(Object.keys(result.meta)).toHaveLength(31);
 });
