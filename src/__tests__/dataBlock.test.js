b261dee68db488310be49d7f45a52b790bf656d4
Luc Patiny <luc@patiny.com>
2021-08-02 14:19:49 +0200

    refactor: dataBlock test

--- src/__tests__/dataBlock.test.js
+++ src/__tests__/dataBlock.test.js
@@ -7,37 +7,55 @@ const { readFileSync } = require('fs');
 
 const { IOBuffer } = require('iobuffer');
 
-const pathFiles = join(__dirname, 'files');
-
-const mXyxyBuffer = new IOBuffer(readFileSync(join(pathFiles, 'm_xyxy.spc')));
-const mXyxy = readDataBlock(mXyxyBuffer, mainHeader(mXyxyBuffer));
-const ramanBuffer = new IOBuffer(readFileSync(join(pathFiles, 'RAMAN.SPC')));
-const raman = readDataBlock(ramanBuffer, mainHeader(ramanBuffer));
-const mOrdZBuffer = new IOBuffer(readFileSync(join(pathFiles, 'm_ordz.spc')));
-const mOrdZ = readDataBlock(mOrdZBuffer, mainHeader(mOrdZBuffer));
-const ftIrBuffer = new IOBuffer(readFileSync(join(pathFiles, 'Ft-ir.spc')));
-const ftIr = readDataBlock(ftIrBuffer, mainHeader(ftIrBuffer));
-const mEvenZBuffer = new IOBuffer(readFileSync(join(pathFiles, 'm_evenz.spc')));
-const mEvenZ = readDataBlock(mEvenZBuffer, mainHeader(mEvenZBuffer));
-test('data block parsing test', () => {
-  expect(mXyxy).toHaveLength(512);
-  expect(mXyxy[0].variables[1].data[1]).toStrictEqual(3188);
-  expect(mXyxy[511].variables[1].data[3]).toStrictEqual(11019);
-  expect(raman[0].variables[0].data[1]).toBeCloseTo(3994.8946331825773);
-  expect(raman[0].variables[1].data[1]).toBeCloseTo(0.0186002254486084);
-  expect(mOrdZ[0].variables[0].data[0]).toStrictEqual(698.229736328125);
-  expect(mOrdZ[0].variables[1].data[0]).toBeCloseTo(0.02219367027282715);
-  expect(mOrdZ[0].variables[1].data[4]).toBeCloseTo(0.005127236247062683);
-  expect(mOrdZ[0].variables[1].data[800]).toBeCloseTo(0.0833737701177597);
-  expect(mOrdZ[0].variables[1].data[856]).toBeCloseTo(0.15000060200691223);
-  expect(mOrdZ[9].variables[1].data[0]).toBeCloseTo(0.023877553641796112);
-  expect(mOrdZ[9].variables[1].data[856]).toBeCloseTo(0.000490216538310051);
-  expect(mOrdZ[9].variables[0].data).toHaveLength(857);
-  expect(mOrdZ[10]).toBeUndefined();
-  expect(ftIr).toHaveLength(1);
-  expect(ftIr[0].variables[1].data[0]).toBeCloseTo(95.13749694824219);
-  expect(mEvenZ[0].variables[0].data).toStrictEqual(
-    mEvenZ[2].variables[0].data,
-  );
-  expect(mEvenZ[6].variables[1].data[4]).toBeCloseTo(0.3032105267047882);
+describe('data block parsing test', () => {
+  it('m_xyxy.spc', () => {
+    const buffer = new IOBuffer(
+      readFileSync(join(__dirname, 'data/m_xyxy.spc')),
+    );
+    const parsed = readDataBlock(buffer, mainHeader(buffer));
+    expect(parsed).toHaveLength(512);
+    expect(parsed[0].variables[1].data[1]).toStrictEqual(3188);
+    expect(parsed[511].variables[1].data[3]).toStrictEqual(11019);
+  });
+  it('RAMAN.SPC', () => {
+    const buffer = new IOBuffer(
+      readFileSync(join(__dirname, 'data/RAMAN.SPC')),
+    );
+    const parsed = readDataBlock(buffer, mainHeader(buffer));
+    expect(parsed[0].variables[0].data[1]).toBeCloseTo(3994.8946331825773);
+    expect(parsed[0].variables[1].data[1]).toBeCloseTo(0.0186002254486084);
+  });
+  it('m_ordz.spc', () => {
+    const buffer = new IOBuffer(
+      readFileSync(join(__dirname, 'data/m_ordz.spc')),
+    );
+    const parsed = readDataBlock(buffer, mainHeader(buffer));
+    expect(parsed[0].variables[0].data[0]).toStrictEqual(698.229736328125);
+    expect(parsed[0].variables[1].data[0]).toBeCloseTo(0.02219367027282715);
+    expect(parsed[0].variables[1].data[4]).toBeCloseTo(0.005127236247062683);
+    expect(parsed[0].variables[1].data[800]).toBeCloseTo(0.0833737701177597);
+    expect(parsed[0].variables[1].data[856]).toBeCloseTo(0.15000060200691223);
+    expect(parsed[9].variables[1].data[0]).toBeCloseTo(0.023877553641796112);
+    expect(parsed[9].variables[1].data[856]).toBeCloseTo(0.000490216538310051);
+    expect(parsed[9].variables[0].data).toHaveLength(857);
+    expect(parsed[10]).toBeUndefined();
+  });
+  it('Ft-ir.spc', () => {
+    const buffer = new IOBuffer(
+      readFileSync(join(__dirname, 'data/Ft-ir.spc')),
+    );
+    const parsed = readDataBlock(buffer, mainHeader(buffer));
+    expect(parsed).toHaveLength(1);
+    expect(parsed[0].variables[1].data[0]).toBeCloseTo(95.13749694824219);
+  });
+  it('should ', () => {
+    const buffer = new IOBuffer(
+      readFileSync(join(__dirname, 'data/m_evenz.spc')),
+    );
+    const parsed = readDataBlock(buffer, mainHeader(buffer));
+    expect(parsed[0].variables[0].data).toStrictEqual(
+      parsed[2].variables[0].data,
+    );
+    expect(parsed[6].variables[1].data[4]).toBeCloseTo(0.3032105267047882);
+  });
 });
