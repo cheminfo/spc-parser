902343d4c388897bf42049906154801800e6fcda
Luc Patiny <luc@patiny.com>
2023-01-30 16:56:35 +0100

    test: add resolutionPro

---
+++ src/__tests__/resolutionPro.test.ts
@@ -0,0 +1,16 @@
+import { readdirSync, readFileSync } from 'fs';
+import { join } from 'path';
+
+import { parse } from '../parse';
+
+test('Resolution pro', () => {
+  const buffer = readFileSync(join(__dirname, 'data', 'resolutionPro.spc'));
+
+  const result = parse(buffer);
+  expect(result.spectra).toHaveLength(1);
+  const variables = result.spectra[0].variables;
+  const min = Math.min(...variables.y.data);
+  const max = Math.max(...variables.y.data);
+  expect(min).toBe(3);
+  expect(max).toBe(205);
+});
